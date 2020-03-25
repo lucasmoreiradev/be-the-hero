70de0260d2b37ee89f10544f81dc94b2b1a7dee5
Lucas Moreira <eskerlucas1@gmail.com>
2020-03-25 18:07:20 -0300

    feat: Add frontend

---
+++ frontend/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Be The Hero</h1>
+    </header>
+  );
+}
